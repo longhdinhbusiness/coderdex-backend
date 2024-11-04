5aa65403b4d3e416eee62d8a65258c8f56683f11
Long Dinh <longhdinhbusiness@gmail.com>
2024-11-04 13:03:58 -0500

    Final draft

--- api/controllers/pokemon/getListPokemons.controller.js
+++ api/controllers/pokemon/getListPokemons.controller.js
@@ -16,7 +16,7 @@ const getListPokemons = (req, res) => {
   }
 
   const pokemonData = readPokemonData();
-  const { type, name, currentPage = 1, pokemonsPerPage = 10 } = value;
+  const { type, name, page = 1, limit = 10 } = value;
 
   let filteredPokemons = pokemonData;
 
@@ -35,8 +35,8 @@ const getListPokemons = (req, res) => {
     );
   }
 
-  const pageNumber = parseInt(currentPage, 10);
-  const limitNumber = parseInt(pokemonsPerPage, 10);
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
   const startIndex = (pageNumber - 1) * limitNumber;
   const endIndex = startIndex + limitNumber;
 
