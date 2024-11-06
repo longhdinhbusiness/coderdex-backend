60953ee72d007dc0747006f0bfcfdf8365a03172
Long Dinh <longhdinhbusiness@gmail.com>
2024-11-05 21:04:29 -0500

    Changed path of routes in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const routes = require('./api/routes/pokemon.route');
+const routes = require('./api/routes/index');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
