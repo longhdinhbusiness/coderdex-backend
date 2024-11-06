4ddd73bb30c0d58c0ccf551c7cc686e1c9bd693b
Long Dinh <longhdinhbusiness@gmail.com>
2024-11-05 19:38:48 -0500

    Final draft

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./api/routes');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}.`);
+  console.log(`Server is running on port : ${PORT}.`);
 });
 
 module.exports = app;
